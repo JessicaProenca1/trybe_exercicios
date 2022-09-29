30f520f42f20e77cd9fe614ed34449c3be2e3027
Jessica <je.proenca@yahoo.com.br>
2022-09-29 19:28:36 -0300

    exercicios foguete

--- fundamentos/secao-04-DOM-eventos-webStorage/buscando-elementos/exercicio-foguete/script3.js
+++ fundamentos/secao-04-DOM-eventos-webStorage/buscando-elementos/exercicio-foguete/script3.js
@@ -7,30 +7,51 @@ tagH1.appendChild(textH1);
 classBody.appendChild(tagH1);
 
 // Adicione a tag main com a classe main-content como filho da tag body;
-let paiBody = document.querySelector('.classBody');
-let addMain = document.createElement('main');
-paiBody.appendChild(addMain);
-addMain.className = ('main-content');
-
-// let paiBody = document.getElementsByClassName('classBody');
+// let paiBody = document.querySelector('.classBody');
 // let addMain = document.createElement('main');
 // paiBody.appendChild(addMain);
 // addMain.className = ('main-content');
 
-
-
-
-
-
-
-
-
+let paiBody = document.getElementsByClassName('classBody')[0];
+let addMain = document.createElement('main');
+paiBody.appendChild(addMain);
+addMain.className = ('main-content');
 
 // Adicione a tag section com a classe center-content como filho da tag main criada no passo 2;
+var paiMain = document.getElementsByClassName('main-content')[0];
+let addSection = document.createElement('section');
+paiMain.appendChild(addSection);
+addSection.className = ('center-content');
+
 // Adicione a tag p como filho do section criado no passo 3 e coloque algum texto;
+let paiSection = document.getElementsByClassName('center-content')[0];
+let addTagP = document.createElement('p');
+paiSection.appendChild(addTagP);
+addTagP.innerText = ('Tentando fazer os exercícios');
+
+
 // Adicione a tag section com a classe left-content como filho da tag main criada no passo 2;
+var paiMain = document.getElementsByClassName('main-content')[0];
+let addSection2 = document.createElement('section');
+paiMain.appendChild(addSection2);
+addSection2.className = ('left-content');
+
 // Adicione a tag section com a classe right-content como filho da tag main criada no passo 2;
+var paiMain = document.getElementsByClassName('main-content')[0];
+let addSection3 = document.createElement('section');
+paiMain.appendChild(addSection3);
+addSection3.className = ('right-content');
+
 // Adicione uma imagem com src configurado para o valor https://picsum.photos/200 e classe small-image. Esse elemento deve ser filho do section criado no passo 5;
+var paiSectionLeftContent = document.getElementsByClassName('left-content')[0];
+let imagem = document.createElement('img');
+img["src"] = 'https://picsum.photos/200';
+paiSectionLeftContent.appendChild(imagem);
+imagem.className = ('small-image');
+
+
+
+
 // Adicione uma lista não ordenada com os valores de 1 a 10 por extenso, ou seja, um, dois, três, … como valores da lista. Essa lista deve ser filha do section criado no passo 6;
 // Adicione 3 tags h3, todas sendo filhas do main criado no passo 2.
 // Agora que você criou muita coisa, vamos fazer algumas alterações e remoções:
