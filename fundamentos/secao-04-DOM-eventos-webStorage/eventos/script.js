b5f17626cd16b87098fb47bed5772252d1851874
Jessica <je.proenca@yahoo.com.br>
2022-10-03 18:18:25 -0300

    parte 8-10

--- fundamentos/secao-04-DOM-eventos-webStorage/eventos/script.js
+++ fundamentos/secao-04-DOM-eventos-webStorage/eventos/script.js
@@ -70,19 +70,93 @@ function displayHolidays(color) {
 displayHolidays('green')
 
 // ! PARTE 5
-function chamarSexta (array){
-let botãoSexta = document.getElementById('btn-holiday').nextElementSibling;
-let listSextas = document.getElementsByClassName('friday');
+function chamarSexta(array) {
+  let botãoSexta = document.getElementById('btn-holiday').nextElementSibling;
+  let listSextas = document.getElementsByClassName('friday');
 
-botãoSexta.addEventListener('click', function (){
-  for (let i = 0; i < listSextas.length; i += 1){
-    if (listSextas[i].innerHTML === 'SEXTOU!'){
-      listSextas[i].innerHTML = array[i];
-    } else {
-      listSextas[i].innerHTML = 'SEXTOU!'
+  botãoSexta.addEventListener('click', function () {
+    for (let i = 0; i < listSextas.length; i += 1) {
+      if (listSextas[i].innerHTML === 'SEXTOU!') {
+        listSextas[i].innerHTML = array[i];
+      } else {
+        listSextas[i].innerHTML = 'SEXTOU!'
+      }
     }
-  }
-});
+  });
+};
+chamarSexta([4, 11, 18, 25]);
+
+// ! PARTE 6
+
+var days = document.getElementsByClassName('day');
+for (i = 0; i < days.length; i += 1) {
+  var lala = days[i]
+  lala.addEventListener('mouseover', function (e) {
+    e.target.style.fontSize = '30px';
+  });
+  lala.addEventListener('mouseout', function (e) {
+    e.target.style.fontSize = '20px';
+  });
 };
 
-chamarSexta([4, 11,18,25]);
+// ! PARTE 7 E 8
+function minhasTarefas(string, color) {
+  let myTasks = document.getElementsByClassName('my-tasks')[0];
+
+  let span = document.createElement('span');
+  myTasks.appendChild(span);
+  span.innerHTML = string;
+
+  let botaoDiv = document.createElement('div');
+  myTasks.appendChild(botaoDiv);
+  botaoDiv.className = 'task';
+  botaoDiv.style.backgroundColor = color;
+};
+minhasTarefas('dar carinho na Pedrita', 'blue');
+// minhasTarefas('estudar', 'red'); para colocar mais uma tarefa preciso colocar isso aqui dentro de um IF/ELSE
+
+// ! PARTE 9
+function selecionarTarefas() {
+  let selecionaBotao = document.getElementsByClassName('task');
+  for (i = 0; i < selecionaBotao.length; i += 1) {
+    x = selecionaBotao[i];
+    x.addEventListener('click', function () {
+      if (x.className == 'task') {
+        x.className = 'task selected';
+      }
+      else {
+        x.className = 'task';
+      }
+    });
+  }
+}
+selecionarTarefas();
+
+// ! PARTE 10
+function corTarefa() {
+  let tarefaSelecionada = document.getElementsByClassName('selected');
+  let taskDiv = document.getElementsByClassName('task')[0];
+  let corTask = taskDiv.style.backgroundColor;
+
+  for (i = 0; i < days.length; i += 1) {
+    let y = days[i]
+    y.addEventListener('click', function (e) {
+      let targetColor = e.target.style.color;
+      if (tarefaSelecionada.length > 0 && targetColor !== corTask) {
+        let color = tarefaSelecionada[0].style.backgroundColor;
+        e.target.style.color = color;
+      }
+      else if (targetColor === corTask) {
+        e.target.style.color = 'rgb(119,119,119)';
+      }
+    });
+  };
+}
+corTarefa();
+
+
+
+
+
+
+
